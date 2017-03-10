1b1531d40ffe7f1ff586d6520f7304de7d461143
Unknown <westixy@gmail.com>
2017-03-10 10:38:42 +0100

    Update

--- visualcalendar.js
+++ visualcalendar.js
@@ -1,21 +1,15 @@
-Number.prototype.toStringN=function(num){
-  let str = this.toString();
-  let diff=num-Math.floor(this).toString().length;
-  for(let i=0 ; i<diff ; i++){
-    str='0'+str;
-  }
-  return str;
-}
-
+Number.prototype.toStringN=function(num){let str = this.toString();let diff=num-Math.floor(this).toString().length;for(let i=0 ; i<diff ; i++){str='0'+str;}return str;}
 Element.parseHTML=function(a,b){if(a instanceof Element)return a;var c=document.createElement('div');if('string'==typeof a){if('<'==a.trim()[0])return c.innerHTML=a,doc=c.children,b?doc:1==doc.length?doc[0]:doc;c.textContent=a;var d=c.firstChild;return b?[d]:d}return c.innerHTML='<![CDATA['+JSON.stringify(a)+']]>',b?[c.firstChild]:c.firstChild},Element.prototype.select=function(a){return this.querySelector(a)},Element.prototype.selects=function(a){return this.querySelectorAll(a)},Element.prototype.parent=function(){return this.parentNode},Element.prototype.prev=function(){return this.previousSibling},Element.prototype.next=function(){return this.nextSibling},Element.prototype.prependx=function(a,b){b||(a=this.constructor.parseHTML(a,!0));var c=a.length,d=this.firstChild;null===d&&this.appendChild(a[0]);for(var e=0;e<c;e++)this.insertBefore(a[0],this.firstChild);return this.firstChild},Element.prototype.appendx=function(a,b){b||(a=this.constructor.parseHTML(a,!0));for(var c=a.length,d=0;d<c;d++)this.appendChild(a[0]);return this.lastChild},Element.prototype.addBefore=function(a,b){b||(a=this.constructor.parseHTML(a,!0));for(var c=this.parentNode,d=a.length,e=0;e<d;e++)c.insertBefore(a[0],this);return this.previousSibling},Element.prototype.addAfter=function(a,b){b||(a=this.constructor.parseHTML(a,!0));var c=this.nextSibling,d=this.parentNode;null===c&&(d.appendChild(a[0]),c=this.nextSibling);for(var e=a.length,f=0;f<e;f++)d.insertBefore(a[0],c);return this.nextSibling},Element.prototype.remove=function(){return this.parentNode.removeChild(this),this},Element.prototype.on=function(a,b){return this.addEventListener(a,b)},Element.prototype.off=function(a,b){return this.removeEventListener(a,b),this};
+// Requirements ^^^^^^^^^^
 
 class VisualCalendar{
+  get version(){return '0.0.1'};
   constructor(config,display=true){
     this.config=null;
     this.builded=null;
     this.selected=[];
     this.ev={
-      onSelect:console.log,
+      onSelect:function(){},
       onUnselect:function(){},
     };
     if(config!==undefined){
@@ -46,9 +40,18 @@ class VisualCalendar{
     if(config.ev!==undefined){
       this.ev.onSelect=config.ev.onSelect||function(){};
       this.ev.onUnselect=config.ev.onUnselect||function(){};
-      this.ev.onMultiple=config.ev.onMultiple||function(){};
     }
 
+  }
+  addPlanif(elem, datetime, className, title, desc){
+    elem.className=className;
+    elem.innerHTML=title;
+    this.config.planified.push({
+      datetime:datetime,
+      type: className,
+      title:title,
+      description:desc
+    });
   }
   build(){
     if(this.config===null) throw new Error('You need to hydrate VisualCalendar');
@@ -61,7 +64,7 @@ class VisualCalendar{
       if(typeof date == 'object' && date[0]!==undefined) {
         var r1 = new Date(date[0]);
         var r2 = new Date(date[1]);
-        while(r1<=r2){ // TODO verifier que ça fonctionner
+        while(r1<=r2){
           dates.push(
             Number.parseInt(r1.getFullYear()).toStringN(4)+'-'+
             Number.parseInt(r1.getMonth()+1).toStringN(2)+'-'+
@@ -94,7 +97,7 @@ class VisualCalendar{
       var hourMax =(Number.parseInt(planifDate.getHours().toString()+planifDate.getMinutes().toStringN(2))+Number.parseInt(this.config.hours.period.replace(':',''))).toStringN(4);
       hourMax=hourMax[0]+hourMax[1]+':'+hourMax[2]+hourMax[3];
       var planifMax=new Date(datetime.getFullYear()+'-'+(datetime.getMonth()+1).toStringN(2)+'-'+datetime.getUTCDate().toStringN(2)+' '+hourMax);
-      console.log(planifDate,datetime,planifMax,datetime.getTime()>=planifDate.getTime() && datetime.getTime()<planifMax.getTime());
+      //console.log(planifDate,datetime,planifMax,datetime.getTime()>=planifDate.getTime() && datetime.getTime()<planifMax.getTime());
       if(
           datetime.getFullYear()==planifDate.getFullYear() &&
           datetime.getMonth()==planifDate.getMonth()&&
